6b9926215815b16590718f61d0509ab761d6513e
Marcio Gurgel <marcio.rga@gmail.com>
2023-04-03 20:30:20 -0300

    Script completo

--- atualiza-dspace.sh
+++ atualiza-dspace.sh
@@ -1,14 +1,52 @@
+#/bin/bash
+
 source ./variaveis-para-atualizacao.properties
-cd $DIRETORIO_DOWNLOAD_FRONTEND
+
+#############################
+## Backups
+#############################
+#
+##########
+## Postgres
+##########
+
+docker run postgres:11 pg_dump --dbname=${POSTGRES_URL_WITHUSERNAME_AND_PASSWORD}  > ./postgres/dump.sql
+
+docker-compose -f dockerfiles/docker-compose-postgres-only.yml up --build -d
+
+##########
+## Diretório de instalação
+##########
+
+cp -r $DSPACE_INSTALL_DIR/config dspace-install-dir
+ln -s $DSPACE_INSTALL_DIR/assetstore dspace-install-dir
+
+
+#########
+# Backend
+#########
+
 wget https://github.com/DSpace/DSpace/archive/refs/tags/dspace-7.5.zip
+
 unzip dspace-7.5.zip
-rm dspace-7.5.zip
+#rm dspace-7.5.zip
 
+mkdir -p source
+mv DSpace-dspace-7.5 source
 
+cp ./dockerfiles/Dockerfile_backend source/DSpace-dspace-7.5/Dockerfile
+cp ./dockerfiles/docker-compose_migration.yml source/DSpace-dspace-7.5/
 
-cd $DIRETORIO_DOWNLOAD_BACKEND
+docker rm -f dspace
+docker rm -f dspacesolr
+docker-compose -f source/DSpace-dspace-7.5/docker-compose_migration.yml up --build -d
+
 wget https://github.com/DSpace/dspace-angular/archive/refs/tags/dspace-7.5.zip
 unzip dspace-7.5.zip
 rm dspace-7.5.zip
+mv dspace-angular-dspace-7.5 source
+rm -rf dspace-7.5
+cp ./dockerfiles/Dockerfile_frontend source/dspace-angular-dspace-7.5/Dockerfile
+cp ./dockerfiles/docker-compose_frontend.yml source/dspace-angular-dspace-7.5/docker/docker-compose.yml
 
-
+docker-compose -f source/dspace-angular-dspace-7.5/docker/docker-compose.yml up --build -d
---
+++ dockerfiles/docker/postgres/scripts/prepara-postgres.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+function cria_usuario_e_bd() {
+    echo "Criando usuário DSpace"
+    psql --username=postgres -c "CREATE USER dspace WITH PASSWORD 'dspace';"
+    psql --username=postgres -c "CREATE DATABASE dspace WITH OWNER dspace;"
+    psql --username=postgres -c "GRANT ALL PRIVILEGES ON DATABASE dspace TO dspace;"
+}
+
+function instala_pg_crypto() {
+    echo "Criando pgCrypto"
+    psql --username=postgres dspace -c "CREATE EXTENSION pgcrypto;"
+}
+
+cria_usuario_e_bd
+instala_pg_crypto
+
+echo "Identificado arquivo de DUMP efetuando a restauração"
+psql -d dspace -U dspace -f /opt/dump.sql
