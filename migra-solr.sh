e53cddbeba8a22b6c36e16c1b55471de263a4c2b
Marcio Gurgel <marcio.rga@gmail.com>
2023-05-23 22:09:48 -0300

    Toque de elegância

--- migra-solr.sh
+++ migra-solr.sh
@@ -1,51 +1,29 @@
 #!/bin/bash
 
 mkdir -p ./dspace-install-dir/solr-conversion-files/libs
+export URL_BASE=https://repo1.maven.org/maven2/org/apache/lucene
+export DEST_FOLDER=./dspace-install-dir/solr-conversion-files/libs
 
-docker run --rm -v $(pwd):/unzip -w /unzip kubeless/unzip \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-core/4.10.4/lucene-core-4.10.4.jar -o ./dspace-install-dir/solr-conversion-files/libs/lucene-core-4.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-backward-codecs/4.10.4/lucene-backward-codecs-4.10.4.jar -o ./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-4.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-core/5.5.4/lucene-core-5.5.4.jar -o ./dspace-install-dir/solr-conversion-files/libs/lucene-core-5.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-backward-codecs/5.5.4/lucene-backward-codecs-5.5.4.jar -o ./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-5.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-core/6.6.0/lucene-core-6.6.0.jar -o ./dspace-install-dir/solr-conversion-files/libs/lucene-core-6.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-backward-codecs/6.6.0/lucene-backward-codecs-6.6.0.jar -o ./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-6.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-core/7.7.3/lucene-core-7.7.3.jar -o ./dspace-install-dir/solr-conversion-files/libs/lucene-core-7.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-backward-codecs/7.7.3/lucene-backward-codecs-7.7.3.jar -o ./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-7.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-core/8.11.1/lucene-core-8.11.1.jar -o ./dspace-install-dir/solr-conversion-files/libs/lucene-core-8.jar -L \
- && curl https://repo1.maven.org/maven2/org/apache/lucene/lucene-backward-codecs/8.11.1/lucene-backward-codecs-8.11.1.jar -o ./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-8.jar -L
+docker run --rm -e $URL_BASE:URL_BASE -e $DEST_FOLDER:DEST_FOLDER -v $(pwd):/unzip -w /unzip kubeless/unzip \
+ && curl $URL_BASE/lucene-core/4.10.4/lucene-core-4.10.4.jar -o $DEST_FOLDER/lucene-core-4.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/4.10.4/lucene-backward-codecs-4.10.4.jar -o $DEST_FOLDER/back-lucene-core-4.jar -L \
+ && curl $URL_BASE/lucene-core/5.5.4/lucene-core-5.5.4.jar -o $DEST_FOLDER/lucene-core-5.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/5.5.4/lucene-backward-codecs-5.5.4.jar -o $DEST_FOLDER/back-lucene-core-5.jar -L \
+ && curl $URL_BASE/lucene-core/6.6.0/lucene-core-6.6.0.jar -o $DEST_FOLDER/lucene-core-6.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/6.6.0/lucene-backward-codecs-6.6.0.jar -o $DEST_FOLDER/back-lucene-core-6.jar -L \
+ && curl $URL_BASE/lucene-core/7.7.3/lucene-core-7.7.3.jar -o $DEST_FOLDER/lucene-core-7.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/7.7.3/lucene-backward-codecs-7.7.3.jar -o $DEST_FOLDER/back-lucene-core-7.jar -L \
+ && curl $URL_BASE/lucene-core/8.11.1/lucene-core-8.11.1.jar -o $DEST_FOLDER/lucene-core-8.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/8.11.1/lucene-backward-codecs-8.11.1.jar -o $DEST_FOLDER/back-lucene-core-8.jar -L
 
-echo "Atualizando indices Solr para versão 4... (aguarde)"
-docker run --rm -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
-  pwd && ls -lsah &&
-  java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-4.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-4.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-4.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-4.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-4.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-4.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-4.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-4.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
-  
-echo "Atualizando indices Solr para versão 5... (aguarde)"
-docker run --rm -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
-  java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-5.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-5.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-5.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-5.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-5.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-5.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-5.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-5.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
-  
-echo "Atualizando indices Solr para versão 6... (aguarde)"
-docker run --rm -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
-  java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-6.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-6.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-6.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-6.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-6.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-6.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-6.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-6.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
-  
-echo "Atualizando indices Solr para versão 7... (aguarde)"
-docker run --rm -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
-  java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-7.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-7.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-7.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-7.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-7.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-7.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-7.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-7.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
-  
-echo "Atualizando indices Solr para versão 8... (aguarde)"
-docker run --rm -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
-  java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-8.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-8.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-8.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-8.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-8.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-8.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
-  && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-8.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-8.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
+
+for version in 4 5 6 7 8
+do
+   echo "Atualizando indices Solr para versão ${version}... (aguarde)"
+   docker run --rm -e $version:version -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
+     java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-${version}.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-${version}.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/authority/data/index \
+     && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-${version}.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-${version}.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/oai/data/index \
+     && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-${version}.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-${version}.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/search/data/index \
+     && java -cp ./dspace-install-dir/solr-conversion-files/libs/lucene-core-${version}.jar:./dspace-install-dir/solr-conversion-files/libs/back-lucene-core-${version}.jar org.apache.lucene.index.IndexUpgrader -delete-prior-commits ./dspace-install-dir/solr/statistics/data/index
+done
+
