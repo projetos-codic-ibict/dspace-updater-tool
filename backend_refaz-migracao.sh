64df56b9466df7681419a10587643893450a6f98
Marcio Gurgel <marcio.rga@gmail.com>
2023-05-27 15:16:57 -0300

    Removendo volume do solr

--- backend_refaz-migracao.sh
+++ backend_refaz-migracao.sh
@@ -14,4 +14,6 @@ docker rm -f dspace7db || true > /dev/null 2>&1
 docker rm -f dspace7solr || true > /dev/null 2>&1
 docker rmi -f ibict/postgresdspace7 || true > /dev/null 2>&1
 
+docker volume rm dspace-dspace-75_solr_data || true > /dev/null 2>&1
+
 ./backend_migra-para-dspace7.sh
