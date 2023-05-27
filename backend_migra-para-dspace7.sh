6a922f21a8cd1a953d32c6d7a2fd1ab7fe04a22e
Marcio Gurgel <marcio.rga@gmail.com>
2023-05-27 14:13:15 -0300

    Nova tentativa de indexação com solr.

--- backend_migra-para-dspace7.sh
+++ backend_migra-para-dspace7.sh
@@ -79,18 +79,33 @@ echo "dspace.server.url = ${BACKEND_PROTOCOL}://${BACKEND_HOSTNAME}:${BACKEND_PO
 echo "dspace.ui.url = ${FRONTEND_PROTOCOL}://${FRONTEND_HOSTNAME}:${FRONTEND_PORT}" >> source/DSpace-dspace-7.5/dspace/config/local.cfg
 
 
-#mkdir -p ./dspace-install-dir/solr-conversion-files/libs
-#cp ./luceneupgrader.jar ./dspace-install-dir/solr-conversion-files/libs
-#
-#export URL_BASE=https://repo1.maven.org/maven2/org/apache/lucene
-#export DEST_FOLDER=./dspace-install-dir/solr-conversion-files/libs
-#
-#
-#for version in 8
-#do
-#   echo "Atualizando indices Solr para versão ${version}... (aguarde)"
-#       java -jar ./dspace-install-dir/solr-conversion-files/libs/luceneupgrader.jar upgrade ./dspace-install-dir/solr/statistics/data/index ${version}
-#done
+mkdir -p ./dspace-install-dir/solr-conversion-files/libs
+cp ./luceneupgrader.jar ./dspace-install-dir/solr-conversion-files/libs
+
+export URL_BASE=https://repo1.maven.org/maven2/org/apache/lucene
+export DEST_FOLDER=./dspace-install-dir/solr-conversion-files/libs
+
+
+docker run --rm -e $URL_BASE:URL_BASE -e $DEST_FOLDER:DEST_FOLDER -v $(pwd):/unzip -w /unzip kubeless/unzip \
+ && curl $URL_BASE/lucene-core/4.10.4/lucene-core-4.10.4.jar -o $DEST_FOLDER/lucene-core-4.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/4.10.4/lucene-backward-codecs-4.10.4.jar -o $DEST_FOLDER/back-lucene-core-4.jar -L \
+ && curl $URL_BASE/lucene-core/5.5.4/lucene-core-5.5.4.jar -o $DEST_FOLDER/lucene-core-5.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/5.5.4/lucene-backward-codecs-5.5.4.jar -o $DEST_FOLDER/back-lucene-core-5.jar -L \
+ && curl $URL_BASE/lucene-core/6.6.0/lucene-core-6.6.0.jar -o $DEST_FOLDER/lucene-core-6.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/6.6.0/lucene-backward-codecs-6.6.0.jar -o $DEST_FOLDER/back-lucene-core-6.jar -L \
+ && curl $URL_BASE/lucene-core/7.7.0/lucene-core-7.7.0.jar -o $DEST_FOLDER/lucene-core-7.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/7.7.0/lucene-backward-codecs-7.7.0.jar -o $DEST_FOLDER/back-lucene-core-7.jar -L \
+ && curl $URL_BASE/lucene-core/8.11.1/lucene-core-8.11.1.jar -o $DEST_FOLDER/lucene-core-8.jar -L \
+ && curl $URL_BASE/lucene-backward-codecs/8.11.1/lucene-backward-codecs-8.11.1.jar -o $DEST_FOLDER/back-lucene-core-8.jar -L
+
+
+for version in 5 6 7
+do
+   echo "Atualizando indices Solr para versão ${version}... (aguarde)"
+   docker run --rm -e $version:version -v $(pwd):/install-dir -w /install-dir adoptopenjdk/openjdk11 \
+       java -jar ./dspace-install-dir/solr-conversion-files/libs/luceneupgrader.jar upgrade ./dspace-install-dir/solr/statistics/data/index
+       java -jar ./dspace-install-dir/solr-conversion-files/libs/luceneupgrader.jar upgrade ./dspace-install-dir/solr/statistics/data/index ${version}
+done
 
 docker compose -f source/DSpace-dspace-7.5/docker-compose_migration.yml up --build -d
 
