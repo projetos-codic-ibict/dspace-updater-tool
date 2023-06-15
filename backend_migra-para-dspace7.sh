5116225b6dda4e285d02fd31788f85c9c4156cdc
Marcio Gurgel <marcio.rga@gmail.com>
2023-06-14 23:24:06 -0300

    Retirada de uso de usuários (-u)

--- backend_migra-para-dspace7.sh
+++ backend_migra-para-dspace7.sh
@@ -79,11 +79,11 @@ rm -rf ./dspace-install-dir/config/spring
 cp -r ./source/DSpace-dspace-7.5/dspace/config/spring ./dspace-install-dir/config/
 # Compile
 mkdir ~/.m2 || true
-docker run -v ~/.m2:/var/maven/.m2 -v "$(pwd)/source/DSpace-dspace-7.5":/tmp/dspacebuild -w /tmp/dspacebuild -ti --rm -u ${UID} -e MAVEN_CONFIG=/var/maven/.m2 maven:3.8.6-openjdk-11 mvn -q --no-transfer-progress -Duser.home=/var/maven clean package -P dspace-oai,\!dspace-sword,\!dspace-swordv2,\!dspace-rdf,\!dspace-iiif
+docker run -v ~/.m2:/var/maven/.m2 -v "$(pwd)/source/DSpace-dspace-7.5":/tmp/dspacebuild -w /tmp/dspacebuild -ti --rm -e MAVEN_CONFIG=/var/maven/.m2 maven:3.8.6-openjdk-11 mvn -q --no-transfer-progress -Duser.home=/var/maven clean package -P dspace-oai,\!dspace-sword,\!dspace-swordv2,\!dspace-rdf,\!dspace-iiif
 ### TODO: give ant a better place
 
 # Ant
-docker run -v ~/.m2:/var/maven/.m2  -v $(pwd)/dspace-install-dir:/dspace  -v $(pwd)/source/DSpace-dspace-7.5:/tmp/dspacebuild -w /tmp/dspacebuild -ti --rm -u ${UID} -e MAVEN_CONFIG=/var/maven/.m2 maven:3.8.6-openjdk-11 /bin/bash -c "wget https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.12-bin.tar.gz && tar -xvzf apache-ant-1.10.12-bin.tar.gz && cd dspace/target/dspace-installer && ../../../apache-ant-1.10.12/bin/ant init_installation update_configs update_code update_webapps && cd ../../../ && rm -rf apache-ant-*"
+docker run -v ~/.m2:/var/maven/.m2  -v $(pwd)/dspace-install-dir:/dspace  -v $(pwd)/source/DSpace-dspace-7.5:/tmp/dspacebuild -w /tmp/dspacebuild -ti --rm  -e MAVEN_CONFIG=/var/maven/.m2 maven:3.8.6-openjdk-11 /bin/bash -c "wget https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.12-bin.tar.gz && tar -xvzf apache-ant-1.10.12-bin.tar.gz && cd dspace/target/dspace-installer && ../../../apache-ant-1.10.12/bin/ant init_installation update_configs update_code update_webapps && cd ../../../ && rm -rf apache-ant-*"
 
 
 docker compose -f source/DSpace-dspace-7.5/docker-compose_migration.yml up --build -d
