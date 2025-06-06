7a4e81fe280262900dbea7ca88df98d06ae44acf
Jesiel Viana <jesielviana@proton.me>
2025-06-06 14:42:05 -0300

    add proxy variables for backend

--- backend_build.sh
+++ backend_build.sh
@@ -112,7 +112,18 @@ printf '
   cat ./local.cfg >source/DSpace-dspace-7.6/dspace/config/local.cfg
   echo "db.password = ${DSPACE_POSTGRES_PASSWORD}" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
   echo "db.url = jdbc:postgresql://dspace7db.dspacenet:5432/dspace" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
-  echo "dspace.server.url = ${BACKEND_PROTOCOL}://${BACKEND_HOSTNAME}:${BACKEND_PORT}/server" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
+
+
+  if [ -n "$REVERSE_PROXY_BACKEND_PROTOCOL" ] || [ -n "$REVERSE_PROXY_BACKEND_HOSTNAME" ] || [ -n "$REVERSE_PROXY_BACKEND_PORT" ]; then
+    if [ -n "$REVERSE_PROXY_BACKEND_PORT" ]; then
+      echo "dspace.server.url = ${REVERSE_PROXY_BACKEND_PROTOCOL}://${REVERSE_PROXY_BACKEND_HOSTNAME}:${REVERSE_PROXY_BACKEND_PORT}" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
+    else
+      echo "dspace.server.url = ${REVERSE_PROXY_BACKEND_PROTOCOL}://${REVERSE_PROXY_BACKEND_HOSTNAME}" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
+    fi
+  else
+     echo "dspace.server.url = ${BACKEND_PROTOCOL}://${BACKEND_HOSTNAME}:${BACKEND_PORT}/server" >>source/DSpace-dspace-7.6/dspace/config/local.cfg
+  fi
+} >>./execution.log 2>&1
 
   if [ -n "$REVERSE_PROXY_FRONTEND_PROTOCOL" ] || [ -n "$REVERSE_PROXY_FRONTEND_HOSTNAME" ] || [ -n "$REVERSE_PROXY_FRONTEND_PORT" ]; then
     if [ -n "$REVERSE_PROXY_FRONTEND_PORT" ]; then
